cd7594aa09ea2768352ee7874bf2414a4e3f290f
Luca Olivetti <luca@ventoso.org>
2014-08-03 01:42:13 +0200

    maxibon: one XHR.poll is better than 2 (isn't it?)

--- luci-svd/files/luasrc/controller/svd/svd.lua
+++ luci-svd/files/luasrc/controller/svd/svd.lua
@@ -11,17 +11,12 @@ function index()
    entry({"admin","svd","channels"}, cbi("svd/channels"),translate("Channels"), 40)
    entry({"admin","svd","accounts"}, cbi("svd/accounts"),translate("Accounts"), 50)
    entry({"admin","svd","dialplan"}, cbi("svd/dialplan"),translate("Dialplan"), 60)
-   entry({"admin", "svd", "account_status"}, call("svd_get_account_status")).leaf = true
-   entry({"admin", "svd", "channels_status"}, call("svd_get_channels_status")).leaf = true
+   entry({"admin", "svd", "status"}, call("svd_get_status")).leaf = true
 end
      
-function svd_get_account_status()
+function svd_get_status()
    luci.http.prepare_content("application/json")
-   luci.http.write(luci.sys.exec("echo get_regs[] | svd_if"))
-end
-
-function svd_get_channels_status()
-   luci.http.prepare_content("application/json")
-   luci.http.write(luci.sys.exec("echo get_chans[] | svd_if"))
+   luci.http.write('{ "regs" : ' ..  luci.sys.exec("echo get_regs[] | svd_if") .. 
+                   ', "chans": ' .. luci.sys.exec("echo get_chans[] | svd_if") .. '}')
 end
                  
